8aec40ea1b51419808ecf0222643005b1ca927d5
Aaron Nguyen and Max Reichman <aaron@>
2017-03-21 15:27:20 -0700

    Add an image when clicking the cat and dog buttons that can then be removed.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,22 @@
 $(document).ready(function() {
-  $("#cat").click(function() {
+  $("#meow").click(function() {
     $("#cat-says").prepend("<li>Meow!</li>");
     $("#dog-says").prepend("<li>Bark!</li>");
+    $("#cat-says").before("<li>Hiss!</li>");
+    $("#dog-says").before("<li>Growl!</li>");
+    $("#cat-says").append($("<img>",{src:"img/cutekittycat.jpg"}));
+    $("img").click(function() {
+      $(this).remove();
+    });
   });
-  $("#dog").click(function() {
+  $("#bark").click(function() {
     $("#cat-says").prepend("<li>Meow!</li>");
     $("#dog-says").prepend("<li>Bark!</li>");
+    $("#cat-says").after("<li>Hiss!</li>");
+    $("#dog-says").after("<li>Growl!</li>");
+    $("#dog-says").append($("<img>",{src:"img/dog.jpg"}));
+    $("img").click(function() {
+      $(this).remove();
+    });
   });
 });
